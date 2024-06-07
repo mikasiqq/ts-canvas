64349d077d0f5e4d013d315e52b3a90460ab1c99
Слинин Глеб Алексеевич <gaslinin@mts.ru>
2024-06-07 03:11:36 +0300

    3 lab

--- src/components/ImageCanvas.tsx
+++ src/components/ImageCanvas.tsx
@@ -1,7 +1,8 @@
 import { observer } from "mobx-react-lite";
-import React, { FC, useEffect, useRef } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import imageCanvasStore from "../stores/image-canvas-store";
 import imageSettingsStore from "../stores/image-settings-store";
+import { Nullable } from "../types";
 import { CanvasPanel } from "./CanvasPanel";
 import css from "./ImageCanvas.module.scss";
 
@@ -11,6 +12,16 @@ interface Props {
 
 export const ImageCanvas: FC<Props> = observer(({ imageUrl }) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState<number>(10);
+  const [startY, setStartY] = useState<number>(10);
+  const [operation, setOperation] = useState<"picker" | "hand">("picker");
+  const [imageObj, setImageObj] = useState<{
+    image?: HTMLImageElement;
+    xposition?: number;
+    yposition?: number;
+  }>({});
+  const [secondColor, setSecondColor] = useState<Nullable<string>>(null);
   const { color, coordinates, setColor, setCoordinates } = imageCanvasStore;
   const {
     scale,
@@ -56,6 +67,7 @@ export const ImageCanvas: FC<Props> = observer(({ imageUrl }) => {
               context
             );
           } else if (measurement === "pixels") {
+            setImageObj({ image: img });
             drawImage(
               {
                 width: (imageSize.width * scale) / 100,
@@ -99,33 +111,109 @@ export const ImageCanvas: FC<Props> = observer(({ imageUrl }) => {
     }
   }, [imageSize, imageUrl, isAspectRatio, measurement, scale, setImageSize]);
 
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    setStartX(0);
+    setStartY(0);
+  };
+
   const handleClick = (
     event: React.MouseEvent<HTMLCanvasElement, MouseEvent>
   ) => {
+    if (operation === "hand") return;
+
     const canvas = canvasRef.current;
     if (!canvas || !canvas.getContext) return;
-
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
     const ctx = canvas.getContext("2d");
-
     if (!ctx) return;
-
     const imageData = ctx.getImageData(x, y, 1, 1);
     const data = imageData.data;
-    setColor(`rgb(${data[0]}, ${data[1]}, ${data[2]})`);
+    if (!event.altKey) {
+      setColor(`rgb(${data[0]}, ${data[1]}, ${data[2]})`);
+    } else {
+      setSecondColor(`rgb(${data[0]}, ${data[1]}, ${data[2]})`);
+    }
     setCoordinates({ x, y });
   };
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      if (!isDragging) return;
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+
+      const rect = canvas.getBoundingClientRect();
+      const dx = event.clientX - rect.left - startX + (imageObj.xposition || 0);
+      const dy = event.clientY - rect.top - startY + (imageObj.yposition || 0);
+
+      setImageObj((prev) => ({
+        ...prev,
+        xposition: dx,
+        yposition: dy,
+      }));
+
+      const ctx = canvas.getContext("2d");
+      ctx?.clearRect(0, 0, canvas.width, canvas.height);
+      if (imageObj.image) {
+        ctx?.drawImage(
+          imageObj.image,
+          dx,
+          dy,
+          imageSize?.width || 0,
+          imageSize?.height || 0
+        );
+      }
+    };
+
+    const handleMouseDown = (event: MouseEvent) => {
+      if (operation === "picker") return;
+
+      setIsDragging(true);
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+      const rect = canvas.getBoundingClientRect();
+      setStartX(event.clientX - rect.left);
+      setStartY(event.clientY - rect.top);
+    };
+
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [
+    imageObj.image,
+    imageObj.xposition,
+    imageObj.yposition,
+    imageSize?.height,
+    imageSize?.width,
+    isDragging,
+    operation,
+    startX,
+    startY,
+  ]);
 
   return (
     <div className={css.container}>
       <canvas ref={canvasRef} onClick={handleClick} />
       <CanvasPanel
+        operation={operation}
         color={color}
+        secondColor={secondColor}
         coordinates={coordinates}
         imageSize={formattedSize}
         onScaleChange={setScale}
+        onChange={setOperation}
       />
     </div>
   );
